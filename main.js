bd6cede9cebc2c1e2d570ccdf2e96235377dd9bf
jib2004 <obasaajibola04@gmail.com>
2023-03-22 09:29:30 +0100

    JS oni pami

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const checkTopBox = document.getElementById('checkbox-top')
 const checkTopDiv = document.getElementById('check-div-top')
 const ulAdd = document.getElementById('todo-reminder').firstElementChild;
 const RemoveLi = document.querySelectorAll('.close-img')
+
+// let todo = JSON.parse(localStorage.getItem('todo')) || []
+
+
 //console.log(ulAdd)
 
 backgroundDisplay.addEventListener('click', ()=>{
@@ -15,23 +19,47 @@ backgroundDisplay.addEventListener('click', ()=>{
 })
 
 const  change= () =>{
-    let myImage = iconDisplay.getAttribute('src')
+    let myImage = iconDisplay.getAttribute('src');
+    let li;
    if(myImage === 'images/icon-sun.svg'){
     console.log('welcome')
-        iconDisplay.setAttribute("src", 'images/icon-moon.svg')
+    iconDisplay.setAttribute("src", 'images/icon-moon.svg')
     document.querySelector('header').style.backgroundImage =  'url(images/bg-desktop-light.jpg)'
     document.querySelector('body').style.backgroundColor =  ' hsl(233, 11%, 84%)'// 
     textBox.style.backgroundColor = 'hsl(0, 0%, 98%)' 
-    document.querySelectorAll('.check-div').style.backgroundColor = 'hsl(0, 0%, 98%)' 
+    textBox.style.borderTopRightRadius = '5px'
+    textBox.style.borderBottomRightRadius = '5px'
+    checkTopDiv.style.backgroundColor = 'hsl(0, 0%, 98%)' 
+    //document.getElementById('adtoit').style.backgroundColor = 'hsl(0, 0%, 98%)'; 
    // checkBox.style.border = 'hsl(236, 33%, 92%)'
+  // checkDiv.forEach(c=> c.style.backgroundColor = 'hsl(0, 0%, 98%)')
+    checkDiv.forEach(li=>{
+        li.style.backgroundColor =  'hsl(0, 0%, 98%)'
+        li.style.color = 'black'
+    })
+    document.getElementById('nav-actions').style.backgroundColor = 'hsl(0, 0%, 98%)'
+    document.getElementById('nav-actions').style.color = 'black'
+
+   // impotantDetails(li)
+    
     }else{
         iconDisplay.setAttribute('src', 'images/icon-sun.svg')
     document.querySelector('header').style.backgroundImage =  'url(images/bg-desktop-dark.jpg)'
     document.querySelector('body').style.backgroundColor =  'hsl(235, 19%, 35%)'
-    textBox.style.backgroundColor = 'hsl(235, 19%, 35%)' 
-    document.querySelector('.check-div').style.backgroundColor = 'hsl(235, 19%, 35%)' 
-    //checkBox.style.border = '1px solid hsl(235, 19%, 35%)'
-    }
+    textBox.style.backgroundColor = 'hsl(235, 19%, 35%)'
+    textBox.style.borderTopRightRadius = '5px'
+    textBox.style.borderBottomRightRadius = '5px' 
+    checkTopDiv.style.backgroundColor = 'hsl(235, 19%, 35%)' 
+    checkDiv.forEach(li=>{
+        li.style.backgroundColor = 'hsl(235, 19%, 35%)'
+        li.style.color = 'hsl(236, 33%, 92%)'
+    })
+    document.getElementById('adtoit').style.backgroundColor = 'hsl(235, 19%, 35%)' ; 
+    document.getElementById('nav-actions').style.backgroundColor = 'hsl(235, 19%, 35%)'
+    document.getElementById('nav-actions').style.color = 'hsl(236, 33%, 92%)'
+   
+   // impotantDetails(li,check,arr)
+}
 
 } 
 
@@ -39,7 +67,7 @@ checkTopDiv.onclick = () =>{
     const checkboxImage = document.getElementById('checkbox-image-top')
     let Image =   checkboxImage.getAttribute('src')
     if (Image === ""){
-        console.log('cacs')
+    
      checkboxImage.setAttribute('src', 'images/icon-check.svg')
      checkTopBox.style.padding = '5px';
     //  checkTopBox.style.background = '';
@@ -67,8 +95,7 @@ textBox.onkeyup = () =>{
 }
 
 const impotantDetails = (li,check,arr) =>{
-    li = document.querySelectorAll('li')
-   // check = document.querySelector(".can") 
+
     if(check.type === 'text'){
         check.type = 'checkbox'
         
@@ -76,34 +103,36 @@ const impotantDetails = (li,check,arr) =>{
     arr = Array.from(ulAdd.children)
     if(arr.length === 20){
         alert('First complete the following quests')
-        return
-      
+        return false;
     }
 }
 
-document.querySelector('form').onsubmit = () =>{
-    let liToRemove = document.querySelector('.check-div')
+document.querySelector('form').onsubmit = (e) =>{
+    e.preventDefault();
+
+
+    
+    let liToRemove = document.querySelector('.liT')
     liToRemove.style.display = 'none';  
    
 
-    const input = document.querySelectorAll('input')
+ 
+    ulAdd.classList.remove('check-div')
+   let task = textBox.value;
+    if(!task){
+        alert('Input a task')
+        return false;
+    }else{
+        console.log('Success')
+    }
 
-    //ulAdd.classList.remove('check-div')
-    let task = textBox.value;
+
     const li = document.createElement('li')
-    ulAdd.append(li)
+
+    li.classList.add('check-div')
+    ulAdd.appendChild(li)
     let  check = document.createElement('input')
     li.appendChild(check).classList.add('can')
-    // check.addEventListener('click', ()=>{
-    //     if(check.checked == false){
-    //         check.checked = true
-    //         console.log('Check')
-    //      }//else{
-    //     //      check.checked = false
-    //     //  }
-    // })
-  
-
     let p = document.createElement('p')
     p.innerText = task
     li.appendChild(p).classList.add('p_remind')
@@ -112,37 +141,28 @@ document.querySelector('form').onsubmit = () =>{
      li.appendChild(img).src = 'images/icon-cross.svg'
      img.style.display = 'block'
     
-     
+    
     let arr = Array.from(ulAdd.children)
-    arr.shift()
+   arr.shift()//It removes the first element of an array// Do not remove this from your code it will affect the items
     let all = document.getElementById('all-number')
-    //console.log(arr)
-    //  arr.length++
-    all.innerText = Number(arr.length) 
+    
+   all.innerText = Number(arr.length) ;
+     
+    console.log(ulAdd)
+
     img.addEventListener('click',(e)=>{
-        let n = arr.length.
-        li.style.display = 'none'    
-        li.remove()
-           if(arr.length > 0){
-             
-             console.log(arr)
-            
-            if(all.innerText == 0){
-                return liToRemove.style.display = 'flex'
-            }
-           }
-          
-            
-         //}
-        // all.innerText = Number(arr.length) - 1
-         //let clickedElement = e.target;
-         
-        // if (clickedElement.classList.contains("can")) {
-        //     arr = arr.filter(x => x !== Number(clickedElement.innerText));
-        //     clickedElement.remove();
-        // }
-
-
+       e.preventDefault();
+        ulAdd.removeChild(li);
+        
+        
+        console.log( arr.indexOf(li))
+        
+        if( arr.indexOf(li) > -1){
+            arr.splice(arr.indexOf(li), 1)
+            console.log(arr)
+        //    all.innerText = Number(arr.length)
+        }
+       
 
 
    })   
@@ -152,30 +172,6 @@ document.querySelector('form').onsubmit = () =>{
 
     impotantDetails(li,check,arr)
    
-    return false;
+   // return false;
 }
 
-let check =document.querySelectorAll('.can')
-check.forEach(i =>{
-    i.addEventListener('click',()=>{
-        console.log('Clicked')
-    })
-})
-
-//  const tick = () =>{
-//     let check =document.querySelectorAll('.can')
-//     check.forEach(t=>{
-//         if(t.checked === true){
-//             console.log('clicked')
-//         }
-//     })
-//      impotantDetails(check)
-//  }
-
-//  tick();
-
-// RemoveLi.forEach(t=>{
-//     t.addEventListener('click' ,()=>{
-//         console.log('Clicked')
-//     })
-// })
